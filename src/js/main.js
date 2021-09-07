fd0ab801d6d22e85dd2dcc8da54647ce914bd6e5
Yongjin Kim <dydwls0669@gmail.com>
2021-09-07 15:30:13 +0900

    main화면 수정

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,6 @@ import webpackImage from '../img/webpack.png';
 window.addEventListener('DOMContentLoaded', () => {
   const el = document.querySelector('#app');
   el.innerHTML = `
-  <h1>Webpack</h1>
+  <h1>Hello, Webpack</h1>
   <img src="${webpackImage}" alt="webpack" />`;
 });
