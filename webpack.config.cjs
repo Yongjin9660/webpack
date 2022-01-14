6a974243d5c5d03d06dc05129cf5f6c839071c5b
yongjin <dydwls0669@gmail.com>
2022-01-14 19:20:33 +0900

    config: file-loader 파일 설정 추가

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,48 +4,48 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [path.resolve(__dirname, 'src/js')],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]?[hash]',
-            },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
-    new HtmlWebpackPlugin({ template: './index.html' }),
-  ],
-  devtool: 'source-map',
-  mode: 'development',
+	entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: [path.resolve(__dirname, 'src/js')],
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]?[hash]',
+						},
+					},
+				],
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+		new HtmlWebpackPlugin({ template: './index.html' }),
+	],
+	devtool: 'source-map',
+	mode: 'development',
 };
